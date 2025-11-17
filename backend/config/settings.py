851f7eb4b3302fa33708ea1b397ab59b7b98599e
timofey <tima.yudin.09@mail.ru>
2025-11-17 13:09:11 +0300

    17.11 2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,15 +95,6 @@ else:
     print("Use PostgreSQL")
 
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
