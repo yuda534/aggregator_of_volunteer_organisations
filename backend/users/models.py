b3023ee9eb23393b6a688333e4696408ccc22dd5
timofey <tima.yudin.09@mail.ru>
2025-12-09 19:18:34 +0300

    09

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-class Ed_org(models.Model):
-    name = models.CharField(max_length=50)
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    school = models.ForeignKey(Ed_org, on_delete=models.CASCADE)
