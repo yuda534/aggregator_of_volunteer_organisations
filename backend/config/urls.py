f6f8d2401219a1becb18ea834d6691487d51225d
timofey <tima.yudin.09@mail.ru>
2025-12-11 17:33:38 +0300

    11 1

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('api/', include('api.urls')),  # основное API
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), 
          name='schema-swagger-ui'),
+    path("", include("api.urls")),
+
 ]
