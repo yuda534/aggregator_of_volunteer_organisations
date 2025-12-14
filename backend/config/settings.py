65bda4bea5437a74d1fd26dbc887592ac9b57699
timofey <tima.yudin.09@mail.ru>
2025-12-14 12:49:58 +0300

    JSON

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
     'drf_yasg',
     'swagger',
+    'rest_framework_simplejwt',
 ]
 
 
@@ -180,3 +181,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import include, path
 from swagger.swagger import schema_view
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # основное API
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), 
          name='schema-swagger-ui'),
     path("", include("api.urls")),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
